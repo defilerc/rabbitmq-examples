9c61e556a6269208d6e858c87db5b4ca46ec7ade
Konstantinos Athanasoglou <kathanasoglou@vivawallet.com>
2019-01-24 06:27:06 +0200

    add init src

---
+++ src/1.HelloWorld/Send/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Text;
+using RabbitMQ.Client;
+
+namespace Send
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.QueueDeclare("hello", false, false, false, null);
+
+                var message = "Hello World!";
+                var body = Encoding.UTF8.GetBytes(message);
+
+                channel.BasicPublish(exchange: "",
+                                    routingKey: "hello",
+                                    basicProperties: null,
+                                    body: body);
+                Console.WriteLine(" [x] Sent {0}", message);
+            }
+
+            Console.WriteLine(" Press [enter] to exit.");
+            Console.ReadLine();            
+        }
+    }
+}
