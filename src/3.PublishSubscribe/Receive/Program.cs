983a727c2bd36bf491356541aae61f210ecd855a
Konstantinos Athanasoglou <kathanasoglou@vivawallet.com>
2019-01-25 06:26:34 +0200

    add publishsubscribe model

---
+++ src/3.PublishSubscribe/Receive/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Text;
+using System.Threading;
+
+using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+
+namespace Receive
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var factory = new ConnectionFactory() { 
+                HostName = "localhost" };
+            using (var connection = factory.CreateConnection())
+            using (var channel = connection.CreateModel())
+            {
+                channel.ExchangeDeclare(
+                    exchange: "logs", type: "fanout");
+
+                var queueName = channel.QueueDeclare().QueueName;
+
+                Console.WriteLine(
+                    $"Generated queue: {queueName}");
+
+                channel.QueueBind(queue: queueName,
+                    exchange: "logs",
+                    routingKey: "");
+
+                Console.WriteLine(" [*] Waiting for logs.");
+
+                var consumer = new EventingBasicConsumer(channel);
+                consumer.Received += (model, ea) =>
+                {
+                    var body = ea.Body;
+                    var message = Encoding.UTF8.GetString(body);
+                    Console.WriteLine(" [x] Received {0}", message);
+
+                    int dots = message.Split('.').Length - 1;
+                    Thread.Sleep(dots * 1000);
+
+                    Console.WriteLine(" [x] Done");
+                };
+
+                channel.BasicConsume(
+                    queue: queueName, autoAck: true, 
+                    consumer: consumer);
+
+                Console.WriteLine(" Press [enter] to exit.");
+                Console.ReadLine();                    
+            }
+        }
+    }
+}
--- src/3.PublishSubscribe/Send/Program.cs
+++ src/3.PublishSubscribe/Send/Program.cs
@@ -13,21 +13,15 @@ namespace Send
             using (var connection = factory.CreateConnection())
             using (var channel = connection.CreateModel())
             {
-                channel.QueueDeclare(queue: "task_queue",
-                     durable: true,
-                     exclusive: false,
-                     autoDelete: false,
-                     arguments: null);
+                channel.ExchangeDeclare(
+                    exchange: "logs", type: "fanout");
 
                 var message = args.Length > 0 ? string.Join(" ", args) : "Hello World";
                 var body = Encoding.UTF8.GetBytes(message);
 
-                var properties = channel.CreateBasicProperties();
-                properties.Persistent = true;
-
-                channel.BasicPublish(exchange: "",
-                     routingKey: "task_queue",
-                     basicProperties: properties,
+                channel.BasicPublish(exchange: "logs",
+                     routingKey: "",
+                     basicProperties: null,
                      body: body);
 
                 Console.WriteLine(" [x] Sent {0}", message);
